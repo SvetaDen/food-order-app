7912f003399f999143a7a1eb79f440d29637fcfa
Sveta Denisyuk <svetlana.denisyuk@gmail.com>
2024-06-17 20:49:42 +0300

    added custom hook for http request

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -15,9 +15,13 @@ const Cart = () => {
     function handleCloseCart() {
         userProgressCtx.hideCart();
     }
+    function handleGoToCheckout() {
+        userProgressCtx.showCheckout();
+    }
 
     return (
-        <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+        <Modal className="cart" open={userProgressCtx.progress === 'cart'}
+               onClose={userProgressCtx.progress === 'cart' ? handleCloseCart :null}>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
@@ -31,7 +35,7 @@ const Cart = () => {
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
                 <Button textOnly onClick={handleCloseCart}>Close</Button>
-                <Button onClick={handleCloseCart}>Go to Checkout</Button>
+                {cartCtx.items.length >0 && (<Button onClick={handleGoToCheckout}>Go to Checkout</Button>) }
             </p>
         </Modal>
     );
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,23 @@
-import React, {useEffect, useState} from 'react';
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
 
+
+const requestConfig ={};
 const Meals = () => {
 
-    const [loadedMeals, setLoadedMeals] = useState([]);
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
+    const {
+        data: loadedMeals,
+        isLoading,
+        error
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-            if (!response.ok) {
-                //....
-            }
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-
-        fetchMeals();
-    }, []);
-
+    if (isLoading) {
+        return <p>Fetching meals...</p>
+    }
 
     return (
         <ul id="meals">
-            {loadedMeals.map((meal)=>(
+            {loadedMeals.map((meal) => (
                 <MealItem key={meal.id} meal={meal}/>
             ))}
         </ul>
