b0dbbca492efefd7193c4afc0bdfbb3e60f25cff
Sveta Denisyuk <svetlana.denisyuk@gmail.com>
2024-08-23 23:11:07 +0300

    delete Procfile

--- backend/app.js
+++ backend/app.js
@@ -66,7 +66,5 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000);
+
