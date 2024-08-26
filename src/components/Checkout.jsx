d0d73a3526079f23fa16aab9c5b8fea69cd94ca1
Sveta Denisyuk <svetlana.denisyuk@gmail.com>
2024-08-27 00:11:52 +0300

    commit

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,7 +7,6 @@ import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import useHttp from "../hooks/useHttp.js";
 import Error from "./Error.jsx";
-import {API_BASE_URL} from "../util/config.js";
 
 const requestConfig = {
     method: 'POST',
@@ -21,7 +20,7 @@ const Checkout = () => {
     const userProgressCtx = useContext(UserProgressContext);
 
     const {data, isLoading: isSending, error, sendRequest,clearData} = useHttp(
-        `${API_BASE_URL}/orders`, requestConfig);
+        'http://localhost:3000/orders', requestConfig);
 
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0);
 
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,7 +1,6 @@
 import MealItem from "./MealItem.jsx";
 import useHttp from "../hooks/useHttp.js";
 import Error from "./Error.jsx";
-import {API_BASE_URL} from "../util/config.js";
 
 
 const requestConfig ={};
@@ -11,7 +10,7 @@ const Meals = () => {
         data: loadedMeals,
         isLoading,
         error
-    } = useHttp(`${API_BASE_URL}/meals`, requestConfig, []);
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
     if (isLoading) {
         return <p className="center">Fetching meals...</p>
